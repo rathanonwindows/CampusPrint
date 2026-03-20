6db910480a2a439a1ddcf0737d8ec29ad686bef9
rathanonwindows <rathanonwindows@gmail.com>
2026-03-20 15:43:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("."));
+app.use(express.static(__dirname));
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+});
 
 /* ================= FILE UPLOAD ================= */
 
